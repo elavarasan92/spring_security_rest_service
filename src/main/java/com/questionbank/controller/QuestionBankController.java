3f7b4d3c1b1dc596c26e53f1cdbdbca85470094f
rameshelava@gmail.com <elavarasan@>
2019-06-05 14:05:31 +0530

    cors issue fix

--- src/main/java/com/questionbank/controller/QuestionBankController.java
+++ src/main/java/com/questionbank/controller/QuestionBankController.java
@@ -27,19 +27,12 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.questionbank.service.QuestionBankService;
 
 @RestController
 @Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class QuestionBankController {
 
 	@Autowired
